953fc2d20a954812468427f2d0d5bb05f00aa272
Damian Tarnawsky <damian@ionic.io>
2022-04-11 15:23:55 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Using IonSearchBar
+This example shows how to use IonSearchBar in a React project.
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,45 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonSearchbar, IonTitle, IonToolbar, IonList, IonItem } from '@ionic/react';
+import { useCallback, useState } from 'react';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [searchText, performSearch] = useState('');
+  const people: Array<string> = ['John Smith', 'Smithers', 'Johnas', 'Smiley Joe', 'Joen Smith', 'John Williams', 'Jeff Joe'];
+
+  const onInputChanged = useCallback((searchText) => {
+    console.log(`performSearch for ${searchText}`);
+    performSearch(searchText);
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Find</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Find</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonSearchbar
+          placeholder="test"
+          onIonChange={e => { onInputChanged(e.detail.value); }}
+          // value={searchInputValue}
+          inputMode="search"
+          className='newChatHeaderStyling'
+          debounce={300}
+        // ref={searchInputRef}
+        ></IonSearchbar>
+        <IonList>
+          {people.filter(name => name.toLowerCase().includes(searchText.toLowerCase())).map(filteredName => (
+            <IonItem>
+              {filteredName}
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
